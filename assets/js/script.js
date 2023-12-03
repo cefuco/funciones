7e116130e118c5ecda1952b6a6201b6adfeab0ec
cefuco <you@example.com>
2023-12-03 10:34:05 -0300

    vvv

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+let ele = document.getElementById("ele1")
+ele.addEventListener("click", () => {
+    pintar(ele)
+});
+
+
+
+function pintar(ele, color = 'yellow') {
+    ele.style.backgroundColor = color
+}
